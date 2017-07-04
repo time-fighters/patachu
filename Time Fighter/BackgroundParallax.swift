3f16b071287f6dcb7eb4548fa326ef6da7842a03
Paulo Henrique Favero Pereira <Paulo@Paulos-MacBook-Pro-2.local>
2017-07-03 22:19:44 -0300

    Increased new assets package. Implemented the background paralax class

---
+++ Time Fighter/BackgroundParallax.swift
@@ -0,0 +1,13 @@
+//
+//  BackgroundParallax.swift
+//  Time Fighter
+//
+//  Created by Paulo Henrique Favero Pereira on 7/1/17.
+//  Copyright © 2017 Fera. All rights reserved.
+//
+
+import UIKit
+
+class BackgroundParallax: SKSpriteNode {
+
+}

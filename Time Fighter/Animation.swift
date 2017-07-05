172a1abac83ee19f75d47da12235d31685535a46
wilson.martins <martins.wilson17@gmail.com>
2017-07-05 08:28:25 -0300

    Adicionando novos Componentes
    Animation será usado para tratar as animacoes das sprites
    Updatable será usado para atualizar a cada frame os nodes necessarios

---
+++ Time Fighter/Animation.swift
@@ -0,0 +1,18 @@
+//
+//  Animation.swift
+//  Time Fighter
+//
+//  Created by Wilson Martins on 04/07/17.
+//  Copyright © 2017 Fera. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+protocol Animate {
+    func animate()
+}
+
+class Animation: NSObject {
+
+}
---
+++ Time Fighter/Updatable.swift
@@ -0,0 +1,18 @@
+//
+//  Updatable.swift
+//  Time Fighter
+//
+//  Created by Wilson Martins on 05/07/17.
+//  Copyright © 2017 Fera. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+protocol Updata {
+    func update()
+}
+
+class Updatable: NSObject {
+
+}

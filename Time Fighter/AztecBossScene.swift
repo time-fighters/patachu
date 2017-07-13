5c4c120261b2ee759cea29a13ef2ca1c436534f6
Paulo Henrique Favero Pereira <Paulo@Paulos-MacBook-Pro-2.local>
2017-07-13 18:13:40 -0300

    Boss scene
    Signed-off-by: Paulo Henrique Favero Pereira <Paulo@Paulos-MacBook-Pro-2.local>

---
+++ Time Fighter/AztecBossScene.swift
@@ -0,0 +1,15 @@
+//
+//  AztecBossScene.swift
+//  Time Fighter
+//
+//  Created by Paulo Henrique Favero Pereira on 7/13/17.
+//  Copyright © 2017 Fera. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+
+class AztecBossScene: SKScene {
+
+}
---
+++ Time Fighter/AztecBossSceneViewController.swift
@@ -0,0 +1,55 @@
+//
+//  AztecBossSceneViewController.swift
+//  Time Fighter
+//
+//  Created by Paulo Henrique Favero Pereira on 7/13/17.
+//  Copyright © 2017 Fera. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class AztecBossSceneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "MainMenu") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+            view.showsPhysics = true
+        }
+    }
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}

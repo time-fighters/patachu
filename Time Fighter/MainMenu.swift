e31c8c923eaed7719267f8c2eeca24ddc80775bd
Paulo Henrique Favero Pereira <Paulo@Paulos-MacBook-Pro-2.local>
2017-07-13 15:14:11 -0300

    Menu implementado
    Signed-off-by: Paulo Henrique Favero Pereira <Paulo@Paulos-MacBook-Pro-2.local>

---
+++ Time Fighter/MainMenu.swift
@@ -0,0 +1,120 @@
+//
+//  MainMenu.swift
+//  Time Fighter
+//
+//  Created by Paulo Henrique Favero Pereira on 7/13/17.
+//  Copyright © 2017 Fera. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class MainMenu: SKScene {
+    var configOpen:Bool = false
+    var soundsOn: Bool = true
+    var configButton: SKSpriteNode?
+    var soundsOnButton: SKSpriteNode?
+    var soundsOffButton: SKSpriteNode?
+    var logo: SKSpriteNode?
+    
+    let buttonsZPositionOn:CGFloat = 5
+    let buttonsZPositionOff:CGFloat = -10
+    
+    override func didMove(to view: SKView) {
+        
+        let button = SKSpriteNode(imageNamed: "playAztecWorld")
+        button.position = CGPoint(x: 0, y: 100)
+        button.name = "playAztecWorld"
+        button.zPosition = 1
+        
+        
+        logo = self.childNode(withName: "Logo") as! SKSpriteNode
+        logo?.zPosition = buttonsZPositionOn
+        
+        configButton = self.childNode(withName: "Config") as! SKSpriteNode
+        configButton?.zPosition = buttonsZPositionOn
+        
+        soundsOnButton = self.childNode(withName: "SoundsOn") as! SKSpriteNode
+        soundsOnButton?.zPosition  = buttonsZPositionOff
+        soundsOffButton = self.childNode(withName: "SoundsOff") as! SKSpriteNode
+        soundsOffButton?.zPosition = buttonsZPositionOff
+        
+        let levelSelectionBackgroung = self.childNode(withName: "FullLevelSelection")
+        levelSelectionBackgroung?.addChild(button)
+    }
+    
+    func createLevelSceneWithLevel(level: String) -> SKScene? {
+        var levelScene: SKScene?
+        switch level {
+        case "playAztecWorld":
+            levelScene = SKScene(fileNamed: "GameScene")
+            print("Gamescene")
+        case "MainMenu":
+            print("Menu")
+        //levelScene = level
+        default: levelScene = nil
+        }
+        
+        levelScene?.scaleMode = .aspectFill
+        
+        return levelScene
+    }
+    
+    func selectLevel(withName level: String) {
+        let fadeTransition = SKTransition.fade(withDuration: 0.3)
+        if let selectedLevel = createLevelSceneWithLevel(level: level) {
+            self.view?.presentScene(selectedLevel, transition: fadeTransition)
+        }
+    }
+    
+    // Not a good idea if you progressively adding new levels,
+    // it's totally depend on how you gonna organize your levels.
+    // Since its level input is not arbitrary, the output of this
+    // rarely nil, if it does, it must be the developer mistake.
+    
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let location = touches.first!.location(in: self)
+        let node =  self.atPoint(location)
+        
+        // If next button is touched, start transition to second scene
+        if (node.name == "playAztecWorld") {
+            selectLevel(withName: node.name!)
+        }else if node.name == "Config" && !configOpen{
+            showSettingButtons()
+        }else if node.name == "Config" && configOpen{
+            hideSettingButtons()
+        }else if node.name == "SoundsOn"{
+            print("Sounds Off")
+            soundsOn = false
+            self.soundsOnButton?.zPosition = buttonsZPositionOff
+            self.soundsOffButton?.zPosition = buttonsZPositionOn
+        }else if node.name == "SoundsOff"{
+            soundsOn = true
+            self.soundsOnButton?.zPosition = buttonsZPositionOn
+            self.soundsOffButton?.zPosition = buttonsZPositionOff
+            
+        }
+        
+    }
+    func showSettingButtons(){
+        configOpen = true
+        self.configButton?.zPosition = buttonsZPositionOn
+        if soundsOn {
+        self.soundsOnButton?.zPosition = buttonsZPositionOn
+        }else{
+            self.soundsOffButton?.zPosition = buttonsZPositionOn
+        }
+    }
+    func hideSettingButtons(){
+        configOpen = false
+        self.configButton?.zPosition = buttonsZPositionOn
+        self.soundsOnButton?.zPosition = buttonsZPositionOff
+        self.soundsOffButton?.zPosition = buttonsZPositionOff
+    }
+    
+    
+}
+
+
---
+++ Time Fighter/MainMenuControllerViewController.swift
@@ -0,0 +1,50 @@
+//
+//  MainMenuControllerViewController.swift
+//  Time Fighter
+//
+//  Created by Paulo Henrique Favero Pereira on 7/13/17.
+//  Copyright © 2017 Fera. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class MainMenuControllerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'MainMenu.sks'
+            if let scene = SKScene(fileNamed: "MainMenu") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            view.showsFPS = true
+            view.showsNodeCount = true
+            view.showsPhysics = true
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
